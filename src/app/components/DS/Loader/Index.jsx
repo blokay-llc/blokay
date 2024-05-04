9778c91ad305a5a85ef47832aaa7baf2a9006c4f
judare <fosebadgame@gmail.com>
2024-05-03 22:24:22 -0500

    loader style

--- src/app/components/DS/Loader/Index.jsx
+++ src/app/components/DS/Loader/Index.jsx
@@ -7,25 +7,12 @@ const AppLoader = ({ size, color = "", className = "" }) => {
     height: size === "sm" ? "1rem" : "80px",
   };
 
-  const divStyle = {
-    width: size === "sm" ? "0.8rem" : "64px",
-    height: size === "sm" ? "0.8rem" : "64px",
-    borderWidth: size === "sm" ? "0.2rem" : "8px",
-  };
-
   const loaderClassName = `lds-ring ${size} ${className}`;
 
   return (
     <div className={loaderClassName} style={loaderStyle}>
       {[1, 2, 3, 4].map((x) => (
-        <div
-          key={x}
-          style={{
-            ...divStyle,
-            borderColor: color,
-            borderTopColor: "transparent",
-          }}
-        />
+        <div key={x} />
       ))}
     </div>
   );
