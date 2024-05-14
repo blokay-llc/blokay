dfa3985cedff2213c3ec39cd639ed2dfb37d45b9
judare <fosebadgame@gmail.com>
2024-05-13 19:51:37 -0500

    responsive login, register

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -32,90 +32,92 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="w-96 ">
-      <a href="/">
-        <img
-          src="/logo-white.svg"
-          className="h-8 mb-10 mx-auto hidden dark:block"
-        />
-        <img src="/logo.svg" className="h-10 mb-10 mx-auto dark:hidden" />
-      </a>
-
-      <form action={login} className="flex flex-col gap-5">
-        <AppInput
-          type="text"
-          label="Email"
-          name="email"
-          value={form.email}
-          onChange={(val: string) => {
-            setForm({ ...form, email: val });
-          }}
-        />
-
-        <AppInput
-          type="password"
-          name="password"
-          label="Password"
-          value={form.password}
-          onChange={(val: string) => {
-            setForm({ ...form, password: val });
-          }}
-        />
-
-        <AppButton
-          text="Sign in"
-          icon="account"
-          type="submit"
-          variant="primary"
-          className="w-full"
-          size="lg"
-          loading={loading}
-        />
-      </form>
-
-      <div></div>
-
-      <div className="mt-5 flex flex-col gap-3">
-        {process.env.NEXT_PUBLIC_GOOGLE_LOGIN && (
-          <div
-            className="transition duration-100 border-2 border-stone-300 dark:border-stone-800 rounded-lg flex gap-3 items-center text-stone-600 px-3 py-2 font-light hover:bg-stone-300 dark:hover:bg-stone-900 dark:text-stone-400 "
-            onClick={() => {
-              loginThird("google");
-            }}
-          >
-            <AppIcon icon="google" className="fill-stone-600 size-5" />
-            <div>Login with Google</div>
-          </div>
-        )}
-
-        {process.env.NEXT_PUBLIC_GITHUB_LOGIN && (
-          <div
-            className="transition duration-100 border-2 border-stone-300 dark:border-stone-800 rounded-lg flex gap-3 items-center text-stone-600 px-3 py-2 font-light hover:bg-stone-300  dark:hover:bg-stone-900 dark:text-stone-400"
-            onClick={() => {
-              loginThird("github");
-            }}
-          >
-            <AppIcon icon="github" className="fill-stone-600 size-5" />
-            <div>Login with GitHub</div>
-          </div>
-        )}
-      </div>
-
-      <div className="text-stone-600 mt-5 font-light text-sm text-center">
-        ¿Any problem to login?
-      </div>
-
-      <div className=" mx-auto mt-10  ">
-        <a
-          href="/register"
-          className="border-stone-300 dark:border-stone-950 border-2 text-stone-700 px-5 py-3 rounded-2xl shadow-2xl shadow-stone-400 dark:shadow-black dark:text-stone-300 hover:bg-stone-200 dark:hover:bg-stone-950 flex items-center gap-5 bg-gradient-to-r from-white dark:from-black dark:to-[#7358bf30] to-indigo-100"
-        >
-          <AppIcon icon="account" className="size-10 fill-stone-500" />
-          <div>
-            <span className="font-bold">¿Need a new workspace? </span>
-            <div className="font-light text-sm">Sign up</div>
-          </div>
+    <div className="container">
+      <div className="lg:max-w-96 mx-auto ">
+        <a href="/">
+          <img
+            src="/logo-white.svg"
+            className="h-8 mb-10 mx-auto hidden dark:block"
+          />
+          <img src="/logo.svg" className="h-10 mb-10 mx-auto dark:hidden" />
         </a>
+
+        <form action={login} className="flex flex-col gap-5">
+          <AppInput
+            type="text"
+            label="Email"
+            name="email"
+            value={form.email}
+            onChange={(val: string) => {
+              setForm({ ...form, email: val });
+            }}
+          />
+
+          <AppInput
+            type="password"
+            name="password"
+            label="Password"
+            value={form.password}
+            onChange={(val: string) => {
+              setForm({ ...form, password: val });
+            }}
+          />
+
+          <AppButton
+            text="Sign in"
+            icon="account"
+            type="submit"
+            variant="primary"
+            className="w-full"
+            size="lg"
+            loading={loading}
+          />
+        </form>
+
+        <div></div>
+
+        <div className="mt-5 flex flex-col gap-3">
+          {process.env.NEXT_PUBLIC_GOOGLE_LOGIN && (
+            <div
+              className="transition duration-100 border-2 border-stone-300 dark:border-stone-800 rounded-lg flex gap-3 items-center text-stone-600 px-3 py-2 font-light hover:bg-stone-300 dark:hover:bg-stone-900 dark:text-stone-400 "
+              onClick={() => {
+                loginThird("google");
+              }}
+            >
+              <AppIcon icon="google" className="fill-stone-600 size-5" />
+              <div>Login with Google</div>
+            </div>
+          )}
+
+          {process.env.NEXT_PUBLIC_GITHUB_LOGIN && (
+            <div
+              className="transition duration-100 border-2 border-stone-300 dark:border-stone-800 rounded-lg flex gap-3 items-center text-stone-600 px-3 py-2 font-light hover:bg-stone-300  dark:hover:bg-stone-900 dark:text-stone-400"
+              onClick={() => {
+                loginThird("github");
+              }}
+            >
+              <AppIcon icon="github" className="fill-stone-600 size-5" />
+              <div>Login with GitHub</div>
+            </div>
+          )}
+        </div>
+
+        <div className="text-stone-600 mt-5 font-light text-sm text-center">
+          ¿Any problem to login?
+        </div>
+
+        <div className=" mx-auto mt-10  ">
+          <a
+            href="/register"
+            className="border-stone-300 dark:border-stone-950 border-2 text-stone-700 px-5 py-3 rounded-2xl shadow-2xl shadow-stone-400 dark:shadow-black dark:text-stone-300 hover:bg-stone-200 dark:hover:bg-stone-950 flex items-center gap-5 bg-gradient-to-r from-white dark:from-black dark:to-[#7358bf30] to-indigo-100"
+          >
+            <AppIcon icon="account" className="size-10 fill-stone-500" />
+            <div>
+              <span className="font-bold">Need a new workspace? </span>
+              <div className="font-light text-sm">Sign up</div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/app/register/components/RegisterForm.tsx
+++ src/app/register/components/RegisterForm.tsx
@@ -37,101 +37,103 @@ export default function RegisterForm() {
   };
 
   return (
-    <div className="w-96 ">
-      <a href="/">
-        <img
-          src="/logo-white.svg"
-          className="h-8 mb-10 mx-auto hidden dark:block"
-        />
-        <img src="/logo.svg" className="h-10 mb-10 mx-auto dark:hidden" />
-      </a>
-
-      <form action={register} className="flex flex-col gap-5">
-        <AppInput
-          type="text"
-          value={form.companyName}
-          label="Company Name"
-          onChange={(val: string) => {
-            setForm({ ...form, companyName: val });
-          }}
-        />
-
-        <AppSelect
-          value={form.companySize}
-          label="Company Size"
-          onChange={(val: string) => {
-            setForm({ ...form, companySize: val });
-          }}
-        >
-          <option value="me">Only me</option>
-          <option value="1-5">1-5</option>
-          <option value="6-20">6-20</option>
-          <option value="21-100">21-100</option>
-          <option value="101-1000">101-1000</option>
-          <option value="1001-infinite">1001-Infinite</option>
-        </AppSelect>
-
-        <AppInput
-          type="text"
-          value={form.name}
-          label="Your name"
-          onChange={(val: string) => {
-            setForm({ ...form, name: val });
-          }}
-        />
-
-        <AppInput
-          type="text"
-          value={form.email}
-          label="Your Email"
-          onChange={(val: string) => {
-            setForm({ ...form, email: val });
-          }}
-        />
-
-        <AppInput
-          type="password"
-          value={form.password}
-          label="Password"
-          onChange={(val: string) => {
-            setForm({ ...form, password: val });
-          }}
-        />
-
-        <div className="text-sm text-stone-500 font-light border-t border-stone-300 dark:border-black mt-3 pt-3">
-          By using Blokay, you are agreeing to our{" "}
-          <a
-            href="https://blokay.com/privacy"
-            className="underline "
-            target="_blank"
-          >
-            privacy policy
-          </a>{" "}
-          and terms of service.
-        </div>
-
-        <AppButton
-          text="Sign up"
-          icon="summaryclient"
-          type="submit"
-          variant="primary"
-          className="w-full"
-          size="lg"
-          loading={loading}
-        />
-      </form>
-
-      <div className=" mx-auto  mt-10  ">
-        <a
-          href="/login"
-          className="border-stone-300 dark:border-stone-950 border-2 text-stone-700 px-5 py-3 rounded-2xl shadow-2xl shadow-stone-400 dark:shadow-black dark:text-stone-300 hover:bg-stone-200 dark:hover:bg-stone-950 flex items-center gap-5 bg-gradient-to-r from-white dark:from-black dark:to-[#7358bf30] to-indigo-100"
-        >
-          <AppIcon icon="account" className="size-10 fill-stone-500" />
-          <div>
-            <span className="font-bold">¿Existing workspace? </span>
-            <div className="font-light text-sm">Sign in</div>
-          </div>
+    <div className="container">
+      <div className="lg:max-w-96 mx-auto ">
+        <a href="/">
+          <img
+            src="/logo-white.svg"
+            className="h-8 mb-10 mx-auto hidden dark:block"
+          />
+          <img src="/logo.svg" className="h-10 mb-10 mx-auto dark:hidden" />
         </a>
+
+        <form action={register} className="flex flex-col gap-5">
+          <AppInput
+            type="text"
+            value={form.companyName}
+            label="Company Name"
+            onChange={(val: string) => {
+              setForm({ ...form, companyName: val });
+            }}
+          />
+
+          <AppSelect
+            value={form.companySize}
+            label="Company Size"
+            onChange={(val: string) => {
+              setForm({ ...form, companySize: val });
+            }}
+          >
+            <option value="me">Only me</option>
+            <option value="1-5">1-5</option>
+            <option value="6-20">6-20</option>
+            <option value="21-100">21-100</option>
+            <option value="101-1000">101-1000</option>
+            <option value="1001-infinite">1001-Infinite</option>
+          </AppSelect>
+
+          <AppInput
+            type="text"
+            value={form.name}
+            label="Your name"
+            onChange={(val: string) => {
+              setForm({ ...form, name: val });
+            }}
+          />
+
+          <AppInput
+            type="text"
+            value={form.email}
+            label="Your Email"
+            onChange={(val: string) => {
+              setForm({ ...form, email: val });
+            }}
+          />
+
+          <AppInput
+            type="password"
+            value={form.password}
+            label="Password"
+            onChange={(val: string) => {
+              setForm({ ...form, password: val });
+            }}
+          />
+
+          <div className="text-sm text-stone-500 font-light border-t border-stone-300 dark:border-black mt-3 pt-3">
+            By using Blokay, you are agreeing to our{" "}
+            <a
+              href="https://blokay.com/privacy"
+              className="underline "
+              target="_blank"
+            >
+              privacy policy
+            </a>{" "}
+            and terms of service.
+          </div>
+
+          <AppButton
+            text="Sign up"
+            icon="summaryclient"
+            type="submit"
+            variant="primary"
+            className="w-full"
+            size="lg"
+            loading={loading}
+          />
+        </form>
+
+        <div className=" mx-auto  mt-10  ">
+          <a
+            href="/login"
+            className="border-stone-300 dark:border-stone-950 border-2 text-stone-700 px-5 py-3 rounded-2xl shadow-2xl shadow-stone-400 dark:shadow-black dark:text-stone-300 hover:bg-stone-200 dark:hover:bg-stone-950 flex items-center gap-5 bg-gradient-to-r from-white dark:from-black dark:to-[#7358bf30] to-indigo-100"
+          >
+            <AppIcon icon="account" className="size-10 fill-stone-500" />
+            <div>
+              <span className="font-bold">Existing workspace? </span>
+              <div className="font-light text-sm">Sign in</div>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
