ebc2a8c9a4f2280c6d049b6e38bc36d3a265ed41
judare <fosebadgame@gmail.com>
2024-07-08 12:56:47 -0500

    update migration mysql script

--- mysql/01-migrate.sql
+++ mysql/01-migrate.sql
@@ -13,7 +13,79 @@ CREATE TABLE `_query_executions` (
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=405 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=809 DEFAULT CHARSET=latin1;
+
+-- Create syntax for TABLE 'block_executions'
+CREATE TABLE `block_executions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `timeMs` int(11) unsigned DEFAULT NULL,
+  `blockId` int(11) unsigned DEFAULT NULL,
+  `userId` int(11) unsigned DEFAULT NULL,
+  `businessId` int(11) unsigned DEFAULT NULL,
+  `dataSourceId` int(11) unsigned DEFAULT NULL,
+  `data` text,
+  `error` varchar(255) DEFAULT NULL,
+  `finishAt` datetime DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `businessId` (`businessId`),
+  KEY `dataSourceId` (`dataSourceId`),
+  KEY `userId` (`userId`),
+  KEY `neuronId` (`blockId`),
+  KEY `createdAt` (`createdAt`)
+) ENGINE=InnoDB AUTO_INCREMENT=34505 DEFAULT CHARSET=latin1;
+
+-- Create syntax for TABLE 'blocks'
+CREATE TABLE `blocks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `cron` varchar(255) DEFAULT NULL,
+  `executions` int(11) unsigned DEFAULT '0',
+  `timeMs` int(20) unsigned DEFAULT '0',
+  `key` varchar(100) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `rolPrivilegeId` int(11) unsigned DEFAULT NULL,
+  `filters` text,
+  `synapse` text,
+  `executable` text,
+  `history` longtext,
+  `businessId` int(11) unsigned DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `businessId` (`businessId`),
+  KEY `type` (`type`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000158 DEFAULT CHARSET=latin1;
+
+-- Create syntax for TABLE 'blocks_logs'
+CREATE TABLE `blocks_logs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `blockId` int(11) unsigned DEFAULT NULL,
+  `userId` int(11) unsigned DEFAULT NULL,
+  `businessId` int(11) unsigned DEFAULT NULL,
+  `synapse` text,
+  `filters` text,
+  `createdAt` date DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `businessId` (`businessId`,`userId`),
+  KEY `neuronId` (`blockId`)
+) ENGINE=InnoDB AUTO_INCREMENT=268 DEFAULT CHARSET=latin1;
+
+-- Create syntax for TABLE 'blog'
+CREATE TABLE `blog` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text CHARACTER SET utf8mb4,
+  `type` varchar(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'businesses'
 CREATE TABLE `businesses` (
@@ -24,13 +96,16 @@ CREATE TABLE `businesses` (
   `ownerId` int(11) unsigned DEFAULT NULL,
   `coreToken` varchar(255) DEFAULT NULL,
   `paymentProviderToken` varchar(255) DEFAULT NULL,
+  `billEmail` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `currentBillId` int(11) unsigned DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `coreToken` (`coreToken`),
   KEY `ownerId` (`ownerId`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'datasources'
 CREATE TABLE `datasources` (
@@ -46,75 +121,7 @@ CREATE TABLE `datasources` (
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `businessId` (`businessId`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-
--- Create syntax for TABLE 'neuron_executions'
-CREATE TABLE `neuron_executions` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `timeMs` int(11) unsigned DEFAULT NULL,
-  `neuronId` int(11) unsigned DEFAULT NULL,
-  `userId` int(11) unsigned DEFAULT NULL,
-  `businessId` int(11) unsigned DEFAULT NULL,
-  `dataSourceId` int(11) unsigned DEFAULT NULL,
-  `data` text,
-  `error` varchar(255) DEFAULT NULL,
-  `finishAt` datetime DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `businessId` (`businessId`),
-  KEY `dataSourceId` (`dataSourceId`),
-  KEY `userId` (`userId`),
-  KEY `neuronId` (`neuronId`)
-) ENGINE=InnoDB AUTO_INCREMENT=9500 DEFAULT CHARSET=latin1;
-
--- Create syntax for TABLE 'neuron_groups'
-CREATE TABLE `neuron_groups` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Create syntax for TABLE 'neuron_logs'
-CREATE TABLE `neuron_logs` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `neuronId` int(11) unsigned DEFAULT NULL,
-  `userId` int(11) unsigned DEFAULT NULL,
-  `businessId` int(11) unsigned DEFAULT NULL,
-  `synapse` text,
-  `filters` text,
-  `createdAt` date DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `businessId` (`businessId`,`userId`),
-  KEY `neuronId` (`neuronId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Create syntax for TABLE 'neurons'
-CREATE TABLE `neurons` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `cron` varchar(255) DEFAULT NULL,
-  `executions` int(11) unsigned DEFAULT '0',
-  `timeMs` int(20) unsigned DEFAULT '0',
-  `key` varchar(100) DEFAULT NULL,
-  `description` varchar(1000) DEFAULT NULL,
-  `rolPrivilegeId` int(11) unsigned DEFAULT NULL,
-  `filters` text,
-  `synapse` text,
-  `executable` text,
-  `history` longtext,
-  `businessId` int(11) unsigned DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `businessId` (`businessId`),
-  KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000115 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'user_permissions'
 CREATE TABLE `user_permissions` (
@@ -127,7 +134,7 @@ CREATE TABLE `user_permissions` (
   PRIMARY KEY (`id`),
   KEY `viewId` (`viewId`),
   KEY `userId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=1204 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1283 DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'users'
 CREATE TABLE `users` (
@@ -152,7 +159,7 @@ CREATE TABLE `users` (
   KEY `businessId` (`businessId`),
   KEY `currentViewId` (`currentViewId`),
   KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'view_groups'
 CREATE TABLE `view_groups` (
@@ -164,14 +171,14 @@ CREATE TABLE `view_groups` (
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `businessId` (`businessId`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 -- Create syntax for TABLE 'view_items'
 CREATE TABLE `view_items` (
   `id` char(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `type` varchar(255) DEFAULT NULL,
   `viewId` int(11) unsigned DEFAULT NULL,
-  `neuronId` int(11) unsigned DEFAULT NULL,
+  `blockId` int(11) unsigned DEFAULT NULL,
   `w` int(11) unsigned DEFAULT NULL,
   `h` int(11) unsigned DEFAULT NULL,
   `options` text,
@@ -182,7 +189,7 @@ CREATE TABLE `view_items` (
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `viewId` (`viewId`),
-  KEY `neuronId` (`neuronId`),
+  KEY `neuronId` (`blockId`),
   KEY `createdAt` (`createdAt`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -193,10 +200,11 @@ CREATE TABLE `views` (
   `slug` varchar(255) DEFAULT NULL,
   `businessId` int(11) unsigned DEFAULT NULL,
   `viewGroupId` int(11) unsigned DEFAULT NULL,
+  `userId` int(11) unsigned DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `viewGroupId` (`viewGroupId`),
   KEY `businessId` (`businessId`)
-) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=123 DEFAULT CHARSET=latin1;
