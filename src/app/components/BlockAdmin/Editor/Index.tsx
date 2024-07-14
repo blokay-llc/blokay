a1691dbf1d89503055249b0d95074bcee7e862ec
judare <fosebadgame@gmail.com>
2024-07-14 13:37:03 -0500

    Add theme settings and fetch types for editor

--- src/app/components/BlockAdmin/Editor/Index.tsx
+++ src/app/components/BlockAdmin/Editor/Index.tsx
@@ -3,6 +3,7 @@ import { useRef, useState, useEffect } from "react";
 import Editor from "@monaco-editor/react";
 import { DS } from "@blokay/react";
 import { updateBlock } from "@/app/services/brain";
+import theme from "./theme.json" assert { type: "json" };
 
 type Props = {
   block: any;
@@ -13,6 +14,7 @@ export default function EditorApp({ block, reload }: Props) {
   const [form, setForm] = useState({ synapse: block?.synapse });
   const [diagnostics, setDiagnostics] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [textTypes, setTextTypes] = useState("");
 
   function handleChange() {
     let code = editorRef.current?.getValue();
@@ -25,6 +27,13 @@ export default function EditorApp({ block, reload }: Props) {
     editorRef.current.onDidChangeModelContent(handleChange);
   }
 
+  function getTypes() {
+    return fetch("/types.d.ts")
+      .then((res) => res.text())
+      .then((txt) => {
+        setTextTypes(txt);
+      });
+  }
   const saveChanges = () => {
     setLoading(true);
     updateBlock({
@@ -41,84 +50,76 @@ export default function EditorApp({ block, reload }: Props) {
   };
 
   function setEditorTheme(monaco: any) {
-    monaco.editor.defineTheme("onedark", {
-      base: "vs-dark",
-      inherit: true,
-      rules: [
-        {
-          token: "comment",
-          foreground: "#5d7988",
-          fontStyle: "italic",
-        },
-        { token: "constant", foreground: "#e06c75" },
-      ],
-      colors: {
-        "editor.background": "#0a0a0a",
-      },
-    });
+    monaco.editor.defineTheme("themeEditor", theme);
+    monaco.editor.setTheme("themeEditor");
+    // monaco.editor.getModels()[0].updateOptions({ tabSize: 4, indentSize: 4 });
 
-    fetch("/types.d.ts")
-      .then((res) => res.text())
-      .then((txt) => {
-        monaco.languages.typescript.typescriptDefaults.addExtraLib(txt, "");
-      });
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(textTypes, "");
   }
 
   useEffect(() => {
     setForm({ ...form, synapse: block?.synapse });
   }, [block]);
 
+  useEffect(() => {
+    getTypes();
+  }, []);
+
+  if (!textTypes || block?.synapse == undefined) {
+    return;
+  }
+
   return (
     <div
-      className=" p-2 rounded-3xl overflow-hidde lg:h-[32rem]"
-      style={{ backgroundColor: "#0a0a0a" }}
+      className="  rounded-3xl overflow-hidde lg:h-[32rem]"
+      style={{ backgroundColor: "#fff" }}
     >
-      {block?.synapse != undefined && (
-        <>
-          {diagnostics.length > 0 && (
-            <div>
-              {diagnostics.map((d: any, k: number) => (
-                <div
-                  key={k}
-                  className="bg-gray-900 text-red-600 text-sm mb-3 rounded-lg py-1 px-3"
-                >
-                  {d.messageText}
-                </div>
-              ))}
+      {diagnostics.length > 0 && (
+        <div>
+          {diagnostics.map((d: any, k: number) => (
+            <div
+              key={k}
+              className="bg-gray-900 text-red-600 text-sm mb-3 rounded-lg py-1 px-3"
+            >
+              {d.messageText}
             </div>
-          )}
+          ))}
+        </div>
+      )}
 
-          <Editor
-            options={{
-              insertSpaces: true,
-              tabSize: 4,
-              fontSize: 14,
-              minimap: {
-                enabled: false,
-              },
-              wordWrap: "on",
-            }}
-            onMount={handleMount}
-            beforeMount={setEditorTheme}
-            theme="onedark"
-            height="100%"
-            defaultLanguage="typescript"
-            value={block.synapse}
+      <Editor
+        options={{
+          insertSpaces: true,
+          tabSize: 4,
+          fontSize: 15,
+          lineHeight: 20,
+          fontWeight: "400",
+          fontFamily: "'JetBrains Mono', 'sans-serif', monospace",
+          minimap: {
+            enabled: false,
+          },
+          wordWrap: "wordWrapColumn",
+          wordWrapColumn: 70,
+        }}
+        onMount={handleMount}
+        beforeMount={setEditorTheme}
+        theme="themeEditor"
+        height="100%"
+        defaultLanguage="typescript"
+        value={block.synapse}
+      />
+      {form.synapse != block.synapse && (
+        <div className="absolute top-1 right-0">
+          <DS.Button
+            text="Guardar"
+            onClick={() => saveChanges()}
+            loading={loading}
+            icon="save"
+            variant="primary"
+            className="w-full"
+            size="sm"
           />
-          {form.synapse != block.synapse && (
-            <div className="absolute top-2 right-0">
-              <DS.Button
-                text="Guardar"
-                onClick={() => saveChanges()}
-                loading={loading}
-                icon="save"
-                variant="primary"
-                className="w-full"
-                size="sm"
-              />
-            </div>
-          )}
-        </>
+        </div>
       )}
     </div>
   );
