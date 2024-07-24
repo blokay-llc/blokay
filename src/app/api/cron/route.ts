325331aa1eed3dbef0b89d82278becf9149cb415
judare <fosebadgame@gmail.com>
2024-07-24 07:39:57 -0500

    Add revalidate and dynamic constants

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import CronExecutor from "./cron-executor";
 let cronExecutor = new CronExecutor();
 
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 export const GET = async function ({}: any) {
   return NextResponse.json({
     data: cronExecutor.getStatus(),
